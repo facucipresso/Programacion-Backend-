81f73f4591a76aa36c8ddda7159cdad0b9e3c14d
Facundo Cipresso <facucipresso@gmail.com>
2022-11-22 19:28:52 -0300

    cambios en el desafio entregable

--- progBackend.js
+++ progBackend.js
@@ -1,40 +1,83 @@
 class ProductManager {
-    constructor(){
+    constructor() {
         this.products = []
     }
 
-    getProducts = ()=>{return this.products}
+    getProducts = () => {
+        return this.products
+    }
 
-    getNextId = ()=>{
-        const count = this.events.lenght
-        
-        if (count > 0){
-            const lastProduct = this.products[count-1]
+    getNextId = () => {
+        const count = this.products.lenght
+
+        if (count > 0) {
+            const lastProduct = this.products[count - 1]
             const id = lastProduct.id + 1
             return id
-        }else {
+        } else {
             return 1
         }
     }
 
-    addProduct = (title, description, price, thumbnail, code, stock )=>{
+
+    getNextCode = () => {
+        const count = this.products.lenght
+
+        if (count > 0) {
+            const lastProduct = this.products[count - 1]
+            const code = lastProduct.code + 1
+            return code
+        } else {
+            return 1
+        }
+    }
+
+
+    addProduct = (title, description, price, thumbnail, stock) => {
         const id = this.getNextId()
-       const product = {
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-        id
-       }
-       this.products.push(product)
+        const code = this.getNextCode()
+        const product = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        /* if (Object.values(product.title) === '') {
+            console.log('Todos los campos son obligarios, por favor llenarlos todos')
+        }else this.products.push(product) */
+        
+        if(title === '')console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else if (description === '') console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else if (price === 0) console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else if (thumbnail === '') console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else if (description === '') console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else if (stock === 0) console.log('Todos los campos son obligarios, por favor llenarlos todos');
+        else this.products.push(product)
+
+        
     }
     
+
+    getProductById = (id) =>{
+      this.products.find(prod => prod.id === id) 
+      if (prod.id != id) console.log('Not found');
+    };
+    
+    
+
 
 }
 
 const productManager = new ProductManager()
-productManager.addProduct('celular', 'lindo celu', 1200, 'lakjñdh',)
+productManager.addProduct("mouse", "raton paranoico", 1200, scr = 'slsll', 22, 2)
+
+productManager.addProduct("teclado","las teclitas", 3000, src = 'lasteclitasdelabuelo', 33, 3)
+
+productManager.addProduct("monitor","el moni moni", 4300, src = 'elmonidelabuelo', 3)
 
 console.log(productManager);
+
